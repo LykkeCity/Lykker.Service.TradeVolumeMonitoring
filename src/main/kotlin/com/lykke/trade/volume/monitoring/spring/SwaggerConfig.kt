79d08797f89b8c63c92f6caafe3f72299de6d673
papchenko <rocketmannot@gmail.com>
2019-02-28 13:08:56 +0200

    LWDEV-10441 basic structure of the project

---
+++ src/main/kotlin/com/lykke/trade/volume/monitoring/spring/SwaggerConfig.kt
@@ -0,0 +1,35 @@
+package com.lykke.trade.volume.monitoring.spring
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+open class SwaggerConfig {
+    @Bean
+    open fun apiDocket(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(ApiInfo(
+                        "Trade volume monitoring service",
+                        "Trade volume monitoring service",
+                        "1.0",
+                        "",
+                        null,
+                        "",
+                        "",
+                        emptyList()
+                ))
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lykke.trade.volume.monitoring.web.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+
+    }
+}
