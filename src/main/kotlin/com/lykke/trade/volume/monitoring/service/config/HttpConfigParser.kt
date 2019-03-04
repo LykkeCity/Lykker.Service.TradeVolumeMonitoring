8d0a3a7cd72c9b96b5ab3662d1b0bdbe3e95473f
papchenko <rocketmannot@gmail.com>
2019-03-04 19:09:27 +0200

    LWDEV-10445 add http config

---
+++ src/main/kotlin/com/lykke/trade/volume/monitoring/service/config/HttpConfigParser.kt
@@ -0,0 +1,68 @@
+package com.lykke.matching.engine.config
+
+import com.google.gson.FieldNamingPolicy.UPPER_CAMEL_CASE
+import com.google.gson.GsonBuilder
+import com.lykke.trade.volume.monitoring.service.config.TradeVolumeConfig
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.FactoryBean
+import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import javax.naming.ConfigurationException
+
+@Component("Config")
+@Profile("default", "!local_config")
+class HttpConfigParser(private val environment: Environment) : FactoryBean<TradeVolumeConfig> {
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger("AppStarter")
+    }
+
+    override fun getObjectType(): Class<*> {
+        return TradeVolumeConfig::class.java
+    }
+
+    override fun getObject(): TradeVolumeConfig {
+        return getConfig()
+    }
+
+    private fun getConfig(): TradeVolumeConfig {
+        val commangLineArgs = environment.getProperty("nonOptionArgs", Array<String>::class.java)
+
+        if (commangLineArgs == null) {
+            LOGGER.error("Not enough args. Usage: httpConfigString")
+            throw IllegalArgumentException("Not enough args. Usage: httpConfigString")
+        }
+
+        return downloadConfig(commangLineArgs[0])
+    }
+
+    private fun downloadConfig(httpString: String): TradeVolumeConfig {
+        val cfgUrl = URL(httpString)
+        val connection = cfgUrl.openConnection()
+        val inputStream = BufferedReader(InputStreamReader(connection.inputStream))
+
+        try {
+            val response = StringBuilder()
+            var inputLine = inputStream.readLine()
+
+            while (inputLine != null) {
+                response.append(inputLine)
+                inputLine = inputStream.readLine()
+            }
+
+            inputStream.close()
+
+            val gson = GsonBuilder().setFieldNamingPolicy(UPPER_CAMEL_CASE).create()
+            return gson.fromJson(response.toString(), TradeVolumeConfig::class.java)
+        } catch (e: Exception) {
+            throw ConfigurationException("Unable to read config from $httpString: ${e.message}")
+        } finally {
+            inputStream.close()
+        }
+    }
+}
