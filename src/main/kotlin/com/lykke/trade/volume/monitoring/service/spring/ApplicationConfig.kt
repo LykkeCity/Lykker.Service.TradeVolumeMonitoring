9c7be2f98fab68fa6faa656ce666a89f8e8e119b
papchenko <rocketmannot@gmail.com>
2019-03-05 10:15:13 +0200

    LWDEV-10445 add http config

---
+++ src/main/kotlin/com/lykke/trade/volume/monitoring/service/spring/ApplicationConfig.kt
@@ -0,0 +1,37 @@
+package com.lykke.trade.volume.monitoring.service.spring
+
+import com.lykke.trade.volume.monitoring.service.config.TradeVolumeConfig
+import com.lykke.utils.config.ConfigInitializer
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.core.env.Profiles
+
+@Configuration
+open class ApplicationConfig {
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(ApplicationConfig::class.java.name)
+    }
+
+    @Bean
+    open fun config(environment: Environment): TradeVolumeConfig? {
+        if (environment.acceptsProfiles(Profiles.of("local_config"))) {
+            return ConfigInitializer.initConfig("local", classOfT = TradeVolumeConfig::class.java)
+        }
+
+        return if(environment.acceptsProfiles(Profiles.of("default"))) {
+            val commandLineArgs = environment.getProperty("nonOptionArgs", Array<String>::class.java)
+            if (commandLineArgs == null) {
+                val errorMessage = "Not enough args. Usage: httpConfigString"
+                LOGGER.error(errorMessage)
+                throw IllegalArgumentException(errorMessage)
+            }
+            ConfigInitializer.initConfig(commandLineArgs[0], classOfT = TradeVolumeConfig::class.java)
+        } else {
+            null
+        }
+    }
+
+}
